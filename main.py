f6e5de6496b383817e374c01cf8cfbe65af100e6
Christina Dan <christinaof94@gmail.com>
2015-04-10 00:08:10 -0500

    Create the .ui file and generated .py file. Wrote a main class. The GUI works now. :)

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.Qt import *
+from ui_mainwindow import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        
+        # Set up the user interface from Designer
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # Make some local modifications to UI
+        self.ui.fileTableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.ui.fileTableWidget.horizontalHeader().setSectionResizeMode(0, QHeaderView.Interactive)
+        self.ui.fileTableWidget.horizontalHeader().setSectionResizeMode(2, QHeaderView.Interactive)
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	m = MainWindow()
+	m.show();
+	sys.exit(app.exec_())
